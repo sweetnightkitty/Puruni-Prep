5f545e3396ce58aaa880d086e2c3db398a0acb72
SaraToth <saratoth101@gmail.com>
2023-08-27 16:58:54 +0900

    Update drop down menus with javascript and create circle time curriculum structure in march

---
+++ javascript.js
@@ -0,0 +1,40 @@
+// Change drop down content for cirlce time or bricks
+
+const select = document.querySelector("#curriculum");
+const para = document.querySelector(".para");
+
+select.addEventListener("change", setCurriculum);
+
+function setCurriculum() {
+    const choice = select.value;
+
+    if (choice === "one") {
+        para.textContent = "Bricks";
+    } else if (choice === "two") {
+        para.textContent = "Circle Time";
+    } else {
+        para.textContent = "";
+    }
+}
+
+
+$('.div-hide-show').hide();
+
+$('#curriculum').change(function () {
+    $('.div-hide-show').hide()    
+    $('#' + this.value).show();
+
+});
+
+// Hide or show content based on curriculum shown;
+
+
+  /*
+  function myFunction() {
+    const content = document.getElementById("aep");
+    if (content.style.display === "none") {
+      content.style.display = "block";
+    } else {
+      content.style.display = "none";
+    }
+  }*/
